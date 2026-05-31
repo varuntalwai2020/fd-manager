2ca3846dd7576fff967c728bf7eb1651be0a3940
Varun Talwai <varuntalwai2020@gmail.com>
2026-05-31 21:32:26 +0530

    PostgreSQL setup

--- config/config/settings.py
+++ config/config/settings.py
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
-
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE' : "django.db.backends.sqlite3",
+            'NAME' : BASE_DIR / "db.sqlite3"
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
