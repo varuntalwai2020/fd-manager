d684e2b0c687d300704e61687eeb660ac3361365
Varun Talwai <varuntalwai2020@gmail.com>
2026-06-01 00:12:01 +0530

    Prevent duplicate reminders

--- config/fdapp/scheduler.py
+++ config/fdapp/scheduler.py
@@ -1,7 +1,11 @@
+# scheduler.py
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from .reminder import check_fd_maturity
 
 def start():
+    print("=== SCHEDULER STARTED ===")
+
     scheduler = BackgroundScheduler()
     scheduler.add_job(check_fd_maturity, 'interval', minutes=1)
     scheduler.start()
